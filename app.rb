311a7d8c2335bdfe5c92dcf0cc8ec046b7028d72
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-14 07:17:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Shana"
+  end
+
+  get '/hometown' do
+    "My hometown is Petaluma"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Hotline Bling'"
+  end
 end
