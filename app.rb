b78795f7920c286bbe4503ebb8742b3e93a230bf
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-14 07:24:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Vanessa"
+  end
+
+  get '/hometown' do
+    "My hometown is Silver Spring, MD"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Forever Young'"
+  end
 end
